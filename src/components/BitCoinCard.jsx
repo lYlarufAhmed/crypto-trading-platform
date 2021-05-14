10267a19422fb501321274c3f79f787a9d6595ea
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-05-14 14:47:36 +0600

    changes in css of Bitcoins part

--- src/components/BitCoinCard.jsx
+++ src/components/BitCoinCard.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 const Container = styled.div`
 display: flex;
-flex: 1 1 30%;
 `
 const CoinImage = styled.img`
 flex: 1 1 30%;
@@ -11,6 +10,7 @@ height: 100%;
 object-fit: contain;
 `
 const InfoContainer = styled(Container)`
+flex: 1 1 70%;
 flex-direction: column;
 `
 const Change = styled.span`
@@ -18,23 +18,24 @@ color: ${props => props.loss ? 'red' : 'green'};
 `
 
 const Card = styled(Container)`
+flex: 1 1 30%;
 border-radius: .6rem;
-box-shadow: 1px 1px 3px 7px lightgrey;
+box-shadow: 1px 1px 3px 3px lightgrey;
 gap: .5rem;
 padding: 1rem 1.5rem;
 `
 
 export default function BitCoinCard(props) {
     return (
-        <Container>
+        // <Container>
             <Card>
                 <CoinImage src={props.iconImage} />
                 <InfoContainer>
-                    <h3>${props.currentPrice}</h3>
+                    <h4>${props.currentPrice}</h4>
                     <p>{props.coinName}</p>
                     <small>Last 24h: <Change loss={props.marketCap < 0}>{props.marketCap}</Change></small>
                 </InfoContainer>
             </Card>
-        </Container>
+        // </Container>
     )
 }
