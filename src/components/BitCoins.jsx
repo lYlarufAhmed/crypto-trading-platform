006c4509e5505e53ae2d0642c9e07f646dceff54
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-05-14 12:48:44 +0600

    initial BitCoins component

---
+++ src/components/BitCoins.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import BitCoinCard from "./BitCoinCard";
+
+const Wrapper = styled.div`
+display: flex;
+gap: .4rem
+`
+
+export default function BitCoins(props){
+    return (
+        <Wrapper>
+            <BitCoinCard/>
+        </Wrapper>
+    )
+
+}
