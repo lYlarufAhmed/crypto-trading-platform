f1515223e7b49ea86184cda8c42862dd4b050b65
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-05-15 21:08:53 +0600

    fixed all the linting issues

--- src/components/HoldingCard.jsx
+++ src/components/HoldingCard.jsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import styled from 'styled-components'
 
 const Card = styled.div`
--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import {useContext} from 'react';
 import styled from 'styled-components'
 import MyCrypto from '../context/MyCrypto';
 import HoldingCard from "./HoldingCard";
@@ -9,17 +9,18 @@ flex-direction: column;
 flex: 1;
 `
 
-export default function Holdings(){
+export default function Holdings() {
     let data = useContext(MyCrypto);
     let coins = Object.entries(data.getHoldings);
-    let currentPrices = [data.getBitcoins.current_price,data.getEthereum.current_price, data.getDogecoin.current_price];
+    let currentPrices = [data.getBitcoins.current_price, data.getEthereum.current_price, data.getDogecoin.current_price];
     console.log(coins);
     return (
         <Container>
             <h2>Current Holdings</h2>
             {
                 coins.map((e,index)=>{
-                   if(e[1].stock > 0) return <HoldingCard coinName={e[0]} holdingObj={e[1]} currentPrice={currentPrices[index]}/>
+                   if(e[1].stock > -1) return <HoldingCard coinName={e[0]} holdingObj={e[1]} currentPrice={currentPrices[index]}/>
+                   else return <></>
                 })
             }
         </Container>
