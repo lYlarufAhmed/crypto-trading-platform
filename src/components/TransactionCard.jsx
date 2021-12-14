98787eb0ea47673e586f3b023ee6e871f595d62b
pickboy900 <ahmedmaruf47@gmail.com>
2021-12-14 11:55:19 +0600

    css issue fixes.

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,27 +1,24 @@
 import styled from 'styled-components'
 
 const Card = styled.div`
-display: flex;
-flex-direction: column;
-flex-wrap: wrap;
-padding: .2rem .4rem;
-border: 1px solid black;
-border-left: 5px solid;
-border-radius: .3rem;
-border-color: ${props=>props.loss ? 'red':'green'};
-height: 4rem;
-justify-content: space-around;
-background-color: white;
-width: 70%;
-padding: 10px 20px;
-height: auto;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  border-radius: .3rem;
+  border: 1px solid ${props => props.loss ? 'red' : 'green'};
+  border-left-width: 5px;
+  justify-content: space-around;
+  background-color: white;
+  width: 70%;
+  padding: 10px 20px;
+  height: auto;
 `
 
-export default function TransactionCard(props){
+export default function TransactionCard(props) {
     return (
         <Card loss={props.action !== "buy"}>
             <p>{props.coin}-{props.ammount}@${props.price}</p>
-            <p>{props.action==="buy"?"Paid: ":"Received: "}{props.ammount*props.price}</p>
+            <p>{props.action === "buy" ? "Paid: " : "Received: "}{props.ammount * props.price}</p>
             <small>Bought on {props.tDate}</small>
         </Card>
     )
